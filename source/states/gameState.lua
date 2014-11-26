32af886e5127a7f3d6a0846449229286b8119456
Gian Hancock <qwertyman@hotmail.com.au>
2014-11-26 18:52:20 +1100

    cleaned up code, escape key now quits

--- source/states/gameState.lua
+++ source/states/gameState.lua
@@ -1,31 +1,36 @@
 local gameState = {}
 
 function gameState:init(lvl)
-	self.timer = 0
-	self.debugString = ""
-	self.enemyList = {}
-	self.spawnList = {}
-	self.projectileList = {}
-	self.spawnIndex = 1
-	self.lvldata = io.open(lvl, 'r')
-	self.background = spr_testBackground
+	self.timer = 0				-- how long this state has been running for
+	self.debugString = ""		-- a string that will be printed on screen
+	self.shipList = {}			-- all enemies currently in the game
+	self.spawnList = {}			-- list of enemies to spawn
+	self.spawnIndex = 1 		-- the index of the next enemy to spawn
+	self.projectileList = {}	-- list of all projectiles in the game
+	self.lvldata = io.open(lvl, 'r')		-- the file holding the level data	
+	self.background = spr_testBackground 	-- the sprite that will be used for the background
+	self.camPos = nil			-- the position of the camera in global coordinates
+	self.projectileCount = 0 	-- the number of projetiles that have been created, serves as an index
+	self.levelSpeed = 50		-- the rate at which the level scrolls  TODO: make this part of the level data
+	self.borderWidth = 200		-- how close to the side of the level the player must be for it to scroll
+	
+	self.camPos = -Vector(-(self.background:getWidth() - love.window.getWidth())/2, love.window.getHeight()) -- set camera pos so that center of map is in center of screen
+
+	-- setup the player
 	self.player = Player:new(self.projectileList)
 	self.player:setPos(Vector(self.background:getWidth()/2, -32))
 	self.player.weapon = Sprayer:new(self.player)
-	self.camPos = -Vector(-(self.background:getWidth() - love.window.getWidth())/2, love.window.getHeight())
-	self.projectileCount = 0
-	self.levelSpeed = 50
-	self.player.constSpeed = Vector.UP*(self.levelSpeed)
-	self.borderWidth = 200
-	table.insert(self.enemyList, self.player)
-	print(self.player.position)
-	
+	self.player.constSpeed = Vector.UP*(self.levelSpeed) 		-- the speed that the player will move up at constantly
+	table.insert(self.shipList, self.player)					-- add player to the shipList
+
 	self:readMap()
 end
 
 function gameState:update(dt)
-	self.camPos = self.camPos - Vector.DOWN*self.levelSpeed*dt
-	-- keep the camera in view
+	self.timer = self.timer + dt
+
+	-- camera scrolling
+	self.camPos = self.camPos - Vector.DOWN*self.levelSpeed*dt 		-- Y scrolling
 	local leftDis = self.player.position.x - self.camPos.x
 	local rightDis = self.camPos.x + love.window.getWidth() - self.player.position.x
 	if (leftDis < self.borderWidth) then
@@ -33,41 +38,52 @@ function gameState:update(dt)
 	elseif (rightDis < self.borderWidth) then
 		self.camPos.x = self.player.position.x + self.borderWidth - love.window.getWidth()
 	end
-
 	self.camPos.x = math.clamp(self.camPos.x, 0, self.background:getWidth() - love.window.getWidth())
 
-
-	self.timer = self.timer + dt
-	for i, v in pairs(self.enemyList) do
+	-- update loops
+	for i, v in pairs(self.shipList) do
 		v:update(dt)
 	end
 	for i, v in pairs(self.projectileList) do
 		v:update(dt)
 	end
 	
+	-- spawn enemies in spawnList
 	if (self.spawnList[self.spawnIndex] and self.timer >= self.spawnList[self.spawnIndex].time) then
-		table.insert(self.enemyList, Enemy:new(self.spawnList[self.spawnIndex].path))
+		table.insert(self.shipList, Enemy:new(self.spawnList[self.spawnIndex].path))
 		self.spawnIndex = self.spawnIndex + 1
 	end
 end
 
 function gameState:draw()
 	love.graphics.translate(-self.camPos.x, -self.camPos.y)
+	self:globalDraw()
+	love.graphics.origin()
+	self:windowDraw()
+end
+
+-- drawing done in global coordinates
+function gameState:globalDraw()
 	love.graphics.draw(self.background, 0, -self.background:getHeight())
 	love.graphics.print(self.timer, 16, 16)
-	for i, v in pairs(self.enemyList) do
+	for i, v in pairs(self.shipList) do
 		v:draw()
 	end
 	for i, v in pairs(self.projectileList) do
 		v:draw()
 	end
-	love.graphics.origin()
+end
+
+-- drawing done in window coordinates (occurs after globalDraw)
+function gameState:windowDraw()
 	love.graphics.print(self.debugString, 16, 16)
 end
 
 function gameState:keypressed(key)
 	if (key == "p") then
 		state:pauseToggle()
+	elseif (key == "escape") then
+		love.event.quit()
 	end
 end
 
